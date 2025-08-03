3716d9c6e834b3630709c38bae2c137eed2e8940
63sara <sara63@me.com>
2025-08-03 14:46:36 +0900

    再提出
    再提出

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -8,9 +8,11 @@
   <p>
     <?php
   $vegetable_mane = ['name' => '玉ねぎ', 'price' => 200, 'place' => '北海道'];
+  $key_translation = ['name' => '名前', 'price' =>'値段', 'place' =>'産地'];
   
   foreach ($vegetable_mane as $key => $value) {
-    echo "{$key}:{$value} <br>";
+    $japanese_key = $key_translation[$key];
+    echo "{$japanese_key}:{$value} <br>";
   }
 
   ?>
