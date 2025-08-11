868f070a907b4e2b263f5dcaddbc614148566845
63sara <sara63@me.com>
2025-08-11 21:11:23 +0900

    課題16を提出
    課題16を提出

---
+++ kadai_016/class.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題_016</title>
+</head>
+<body>
+<?php
+
+//Foodクラスを定義する
+class Food {
+
+  //プロパティを定義する
+  private $name;
+  private $price;
+
+  //メソッドを定義する
+  public function show_price() {
+    echo $this->price;
+  } 
+
+  //コンストラクタを定義する
+  public function __construct(string $name, int price) {
+    $this->name = $name;
+    $this->price = $price;
+  }
+
+}
+
+//インスタンス化
+$food = new Food('potato', 250);
+
+
+//Animalクラスを定義する
+class Animal {
+
+  //プロパティを定義する
+  private $name;
+  private $height;
+  private $weight;
+
+  //メソッドを定義する
+  public function show_height() {
+    echo $this->height;
+  }
+
+  //コンストラクタを定義する
+  public function __construct(string $name, int $height, int $weight) {
+    $this->name = $name;
+    $this->height = $height;
+    $this->weight = $weight;
+  }
+
+}
+
+//インスタンス化
+$animal = new Animal('dog', 60, 5000);
+
+//メソッドにアクセスしメソッドを実行する
+echo '<br>';
+$food->show_price();
+
+echo  '<br>';
+$animal->show_height();
+?>
+</body>
+</html>
